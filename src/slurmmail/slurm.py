a8c03b95ef3406f8b93454d827958e6433d89706
David Murray <17888324+dajamu@users.noreply.github.com>
2026-09-06 16:52:28 +0100

    refactor: Add new end_dt and start_dt properties to the Job class
    It is convenient to have direct access to a datetime object for start and end
    times. Added this function to allow for faster access for timedelta operations.
    Updated the existing start and end functions to make use of it.

--- src/slurmmail/slurm.py
+++ src/slurmmail/slurm.py
@@ -165,9 +165,17 @@ class Job:
 
     @property
     def end(self) -> str:
-        if self.end_ts is None:
+        try:
+            return self.end_dt.strftime(self.__datetime_format)
+        except Exception:
             return "N/A"
-        return datetime.fromtimestamp(self.end_ts).strftime(self.__datetime_format)
+
+    @property
+    def end_dt(self) -> Optional[datetime]:
+        try:
+            return datetime.fromtimestamp(self.end_ts)
+        except Exception:
+            return None
 
     @property
     def end_ts(self) -> Optional[int]:
@@ -202,9 +210,17 @@ class Job:
 
     @property
     def start(self) -> str:
-        if self.start_ts is None:
+        try:
+            return self.start_dt.strftime(self.__datetime_format)
+        except Exception:
             return "N/A"
-        return datetime.fromtimestamp(self.start_ts).strftime(self.__datetime_format)
+
+    @property
+    def start_dt(self) -> Optional[datetime]:
+        try:
+            return datetime.fromtimestamp(self.start_ts)
+        except Exception:
+            return None
 
     @property
     def start_ts(self) -> Optional[int]:
